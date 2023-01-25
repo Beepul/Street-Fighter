adab5babf683a8ac2f6b5c747f63e1d8960e14f5
Beepul Magar <79615064+Beepul@users.noreply.github.com>
2023-01-25 11:56:20 +0545

    Add files via upload

---
+++ fightingGame/script.js
@@ -0,0 +1,148 @@
+let playButton = document.getElementById('play')
+let resultDiv = document.getElementById('result')
+let p1NameDiv = document.getElementById('p1Name')
+let p2NameDiv = document.getElementById('p2Name')
+let p1HealthDiv = document.getElementById('p1Health')
+let p2HealthDiv = document.getElementById('p2Health')
+
+
+const updateGame = (p1,p2,gameState) => {
+    p1NameDiv.innerText = p1.name;
+    p2NameDiv.innerText = p2.name;
+    p1HealthDiv.innerText = p1.health
+    p2HealthDiv.innerText = p2.health
+//    console.log('outside',gameState)
+    if(p1.health <= 0 || p2.health <=0){
+        game.isOver = true;
+        gameState = game.isOver
+        // console.log('inside',gameState)
+        // console.log(game.declareWinner(game.isOver,p1,p2))
+        resultDiv.innerText = game.declareWinner(gameState,p1,p2)
+        return gameState
+    }
+}
+
+class Player {
+  constructor(name, health, attackDamage) {
+    this.name = name;
+    this.health = health;
+    this.attackDmg = attackDamage;
+  }
+
+  strike (player, enemy, attackDmg) {
+    let damageAmount = Math.ceil(Math.random() * attackDmg)
+    enemy.health -= damageAmount
+    updateGame(p1,p2,gameState)
+    return `${player.name} attacks ${enemy.name} for ${damageAmount} damage!`
+  }
+
+  heal (player) {
+      if(player.health >= 100){
+          return
+        }
+    let hpAmount = Math.ceil(Math.random() * 5)
+    player.health += hpAmount
+    updateGame(p1,p2,gameState)
+    return `${player.name} heals for ${hpAmount} hp!`
+  }
+}
+
+class Game {
+  constructor() {
+    this.isOver = false;
+  }
+
+  declareWinner(isOver,p1, p2) {
+    let message 
+    if(isOver == true && p1.health <= 0) {
+        message = `${p2.name} WINS !`
+        
+    }else if (isOver == true && p2.health <= 0) {
+        message = `${p1.name} WINS !`
+        // document.getElementById('victory').play()
+    }
+    document.getElementById('victory').play()
+    
+    return message
+  }
+
+  reset(p1,p2) {
+    p1.health = 100
+    p2.health = 100
+    this.isOver = false
+    resultDiv.innerText = ''
+    updateGame(p1,p2,this.isOver)
+  }
+  
+
+  play(p1, p2) {
+    this.reset(p1,p2);
+    while (!this.isOver) {
+      p1.strike(p1,p2, p1.attackDmg)
+      p2.heal(p2)
+      p2.strike(p2,p1, p2.attackDmg);
+      p1.heal(p1)
+    }
+    return this.declareWinner(this.isOver,p1,p2)
+  }
+
+}
+
+let player1 = new Player('Bipul', 100,10)
+// console.log(player1)
+let player2 = new Player('Dipesh', 100,10)
+// console.log(player2)
+
+let p1 = player1;
+let p2 = player2
+
+let game = new Game();
+updateGame(p1,p2,game)
+
+// console.log(game)
+let gameState;
+
+
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'q' && p2.health > 0 && game.isOver == false){
+        p1.strike(p1,p2,p1.attackDmg)
+        document.getElementById('p1attack').play()
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'a' && p2.health > 0 ){
+        p1.heal(p1)
+        document.getElementById('p1heal').play()
+    }
+});
+
+// ** Player 2 Controls **
+document.addEventListener('keydown', function(e) {
+  
+  if(e.key == 'p' && p1.health > 0 && game.isOver == false){
+        p2.strike(p2,p1,p2.attackDmg)
+        document.getElementById('p2attack').play()
+    }
+
+});
+
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'l' && p1.health > 0 ){
+        p2.heal(p2)
+        document.getElementById('p2heal').play()
+    }
+
+});
+
+play.onclick = () =>  game.play(p1,p2)
+
+// console.log(p1.strike(p1,p2,p1.attackDmg))
+// console.log(p2.strike(p2,p1,p2.attackDmg))
+// console.log(p1.heal(p1))
+// console.log(p2.heal(p2))
+// console.log(game.declareWinner(game.isOver,p1,p2))
+
+
+
+
